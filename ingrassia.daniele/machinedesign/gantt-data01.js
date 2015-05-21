70758795e99ff74255a648ccbd6a424a5d9455c3
Massimo Menichinelli <info@openp2pdesign.org>
2015-05-21 16:28:10 +0200

    Update + Gantt files for Machine Design exercise

---
+++ ingrassia.daniele/machinedesign/gantt-data01.js
@@ -0,0 +1,16 @@
+var tasks = {
+	"data":[
+        {id:1, text:"Project #2", start_date:"10-05-2015", duration:18, 
+    progress:0.4, open: true, manager: "Daniele"},
+        {id:2, text:"Task #1",    start_date:"15-05-2015", duration:8, 
+    progress:0.6, parent:1, manager: "Simone"},
+        {id:3, text:"Task #2",    start_date:"21-05-2015", duration:8, 
+    progress:0.6, parent:1, manager: "Enrico"}
+    ],
+    links:[
+        { id:1, source:1, target:2, type:"1"},
+        { id:2, source:2, target:3, type:"0"},
+        { id:3, source:3, target:4, type:"0"},
+        { id:4, source:2, target:5, type:"2"},
+  ]
+}
