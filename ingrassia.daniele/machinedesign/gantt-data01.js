f8f1603e42e06e1204f501813e933b3afa8202fc
Massimo Menichinelli <info@openp2pdesign.org>
2015-05-21 16:46:27 +0200

    Improve Gantt files

--- ingrassia.daniele/machinedesign/gantt-data01.js
+++ ingrassia.daniele/machinedesign/gantt-data01.js
@@ -1,11 +1,11 @@
 var tasks = {
 	"data":[
         {id:1, text:"Project #2", start_date:"10-05-2015", duration:18, 
-    progress:0.4, open: true, manager: "Daniele"},
+    progress:0.4, open: true, workers: "Daniele"},
         {id:2, text:"Task #1",    start_date:"15-05-2015", duration:8, 
-    progress:0.6, parent:1, manager: "Simone"},
+    progress:0.6, parent:1, workers: "Simone"},
         {id:3, text:"Task #2",    start_date:"21-05-2015", duration:8, 
-    progress:0.6, parent:1, manager: "Enrico"}
+    progress:0.6, parent:1, workers: "Enrico"}
     ],
     links:[
         { id:1, source:1, target:2, type:"1"},
