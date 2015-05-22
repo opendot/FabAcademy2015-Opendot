7e59cf8a5bd9df33cadf27c90eedcaa921a6db08
Massimo Menichinelli <info@openp2pdesign.org>
2015-05-22 13:07:01 +0200

    Add description+tooltip in Gantt

--- ingrassia.daniele/machinedesign/gantt-data01.js
+++ ingrassia.daniele/machinedesign/gantt-data01.js
@@ -1,11 +1,11 @@
 var tasks = {
 	"data":[
         {id:1, text:"Project #2", start_date:"10-05-2015", duration:18, 
-    progress:0.4, open: true, workers: "Daniele"},
+    progress:0.4, open: true, workers: "Daniele", description: "Add here a long description"},
         {id:2, text:"Task #1",    start_date:"15-05-2015", duration:8, 
-    progress:0.6, parent:1, workers: "Simone"},
+    progress:0.6, parent:1, workers: "Simone", description: "Add here a long description"},
         {id:3, text:"Task #2",    start_date:"21-05-2015", duration:8, 
-    progress:0.6, parent:1, workers: "Enrico"}
+    progress:0.6, parent:1, workers: "Enrico", description: "Add here a long description"}
     ],
     links:[
         { id:1, source:1, target:2, type:"1"},
